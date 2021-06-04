f334365e9a3db27797018889e4f8c1fafc5702a0
AlexBr0206 <84537155+AlexBr0206@users.noreply.github.com>
2021-06-04 14:54:10 -0500

    Update HomeController.java

--- src/main/java/com/proyecto/pac/controller/HomeController.java
+++ src/main/java/com/proyecto/pac/controller/HomeController.java
@@ -2,9 +2,12 @@ package com.proyecto.pac.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
+	
+	private IProductoService productoService;
 
 	@GetMapping({"/index","/home","/",})
 	public String index() {
